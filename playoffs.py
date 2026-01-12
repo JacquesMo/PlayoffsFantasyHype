932a6dde8dc3913c4fb5bdce5e35ee8e6e6d09e7
JacquesMo <jacquesimosseri@gmail.com>
2026-01-12 11:04:06 -0500

    Update eliminated teams list
    Removed 'GB' from the list of eliminated teams.

--- playoffs.py
+++ playoffs.py
@@ -23,7 +23,7 @@ PLAYOFF_ROUNDS = ["Wild Card", "Divisional", "Conference Championship", "Super B
 # --- ELIMINATED TEAMS ---
 # ADMIN: Add the 2 or 3 letter abbreviation of eliminated teams here to highlight them in red.
 # Example: ["MIA", "NYG", "CLE"]
-ELIMINATED_TEAMS = ["GB", "JAX", "CAR", "LAC", "PHI"] 
+ELIMINATED_TEAMS = ["JAX", "CAR", "LAC", "PHI"] 
 
 # --- NAME MAPPER ---
 # Ensuring nicknames match official API LongNames
